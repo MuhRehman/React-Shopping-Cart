7772f30fb2fb7e49548e19c9a25ad43e9e0b438b
MuhRehman <paki_jan@yahoo.com>
2020-10-28 12:33:34 -0700

    Updates

---
+++ precache-manifest.83f5354eb13f31817d8b84c710a052fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c97e34cd47f360b738d88cc101bd4d2",
+    "url": "/React-Shopping-Cart/index.html"
+  },
+  {
+    "revision": "5f01ea3afe86bd70723b",
+    "url": "/React-Shopping-Cart/static/css/main.8f6d1047.chunk.css"
+  },
+  {
+    "revision": "60adf38556c9ac64232a",
+    "url": "/React-Shopping-Cart/static/js/2.b2eadc8f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React-Shopping-Cart/static/js/2.b2eadc8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f01ea3afe86bd70723b",
+    "url": "/React-Shopping-Cart/static/js/main.d0269de5.chunk.js"
+  },
+  {
+    "revision": "a00a01e409237226fbb2",
+    "url": "/React-Shopping-Cart/static/js/runtime-main.ba468da7.js"
+  }
+]);
